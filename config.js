a65ef49ad42a361f211a7bb237d65494b1e881c6
Ryan Rankin <ryrankin@gmail.com>
2017-02-26 09:40:42 -0500

    third commit

---
+++ config.js
@@ -0,0 +1,10 @@
+exports.DATABASE_URL = 'mongodb://ryrankin:Mn7u5g5a223@ds161179.mlab.com:61179/note-cap' ||
+				process.env.DATABASE_URL ||
+				global.DATABASE_URL ||
+				'mongo://localhost/note-cap';
+
+exports.TEST_DATABASE_URL = (
+	process.env.TEST_DATABASE_URL ||
+	'mongodb://localhost/test-note-cap');
+
+exports.PORT = process.env.PORT || 8080;
