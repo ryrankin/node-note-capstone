7dc153c35c6879decad18dfbd7d0a37cd125554b
Ryan Rankin <ryrankin@gmail.com>
2017-03-01 09:57:15 -0500

    api

--- client/api.js
+++ client/api.js
@@ -1,38 +1,49 @@
-var BASE_URL = 'https://obscure-ridge-73322.herokuapp.com';
+var SEARCHTERM ='';
+var RUNCALL = true;
 
-/*function getDataFromApi(searchTerm, callback) {
-  var settings = {
-    url: BASE_URL,
-    notes: '',
-    dataType: 'json',
-    type: 'GET',
-    success: callback
-  };
-  $.ajax(settings);
-}*/
+var getDataFromApi = function(searchTerm){
+	var query = {
+		method: ['GET', 'PUT', 'POST', 'DELETE'],
+		fields: ['id', 'title', 'content', 'date'],
+	};
+	$.ajax({
+		url: 'https://obscure-ridge-73322.herokuapp.com/',
+		data: query,
+		success: function(data, e){
+			displayNotes(data);
+		},
+		dataType: 'json'
+	});
 
-function getSearchResults(data){
-  return $.ajax({
-    method: 'GET',
-    url: '/',
-    data: data
-  });
+
+function displayNotes(notes){
+	var html = "";
+	if(notes.notes.length === 0){
+		RUNCALL = false;
+		html += '<p>No search results for ' + SEARCHTERM + '</p>'
+	} else {
+		$.each(notes.notes, function(index, value){
+			html += '<li>' + value.title + '</li></br>';
+			html += '<li>' + value.content + '</li><br>';
+			html += '<li>' + value.date + '</li><br>';
+		});
+	}
+	$('.js-search-form').append(html);
+	$('.js-query').val('');
+	$('.js-search-results').show();
 }
 
+$(document.ready(function(){
+	$('.js-search-results').hide();
 
-
-function displaySearchData(Array) {
-  var resultElement = '';
-  console.log(data);
-  if (data.Array.title) {
-    console.log(array.title);
-    Array.title.forEach(function(item) {
-     resultElement += '<p>' + Array.title + '</p>';
-    });
-  }
-  else {
-    resultElement += '<p>No results</p>';
-  }
-  
-  $('.js-search-results').html(resultElement);
+	$('api-search').submit(function(e){
+		e.preventDefault();
+		if( $('.js-query').val.length === 0) {return false};
+		$('.js-search-results').html('');
+		SEARCHTERM = $('.js-query').val();
+		$('.js-search-results').text(SEARCHTERM);
+		getDataFromApi(SEARCHTERM);
+		RUNCALL = true;
+	})
+}))
 }
