17d99086c02ae0ebceea035f0f06ae6fcb3d5eb7
Ryan Rankin <ryrankin@gmail.com>
2017-03-14 18:48:58 -0400

    CI tests

--- test/test-note-app.js
+++ test/test-note-app.js
@@ -56,7 +56,63 @@ describe ('notes API resource', function(){
 
 	describe('GET endpoint', function(){
 
+		it('should return all existing note posts', function(){
 
+			let res;
+			return chai.request(app)
+			.get('/notes')
+			.then(function(_res){
+				res = _res;
+				res.should.have.status(200);
+				res.body.should.have.length.of.at.least(1);
+				return Notes.count();
+				})
+			.then(function(count){
+				res.body.should.have.length.of(count);
+			});
+		});
+
+		it('should return notes with right fields', function(){
+
+			let resNotes;
+			return chai.request(app)
+			.get('/notes')
+			.then(function(res){
+				res.should.have.status(200);
+				res.should.be.json;
+				res.should.be.a('array');
+				res.body.should.have.length.of.at.least(1);
+
+				res.body.forEach(function(notes){
+					notes.should.be.a('object');
+					notes.should.include.keys('id', 'title', 'content', 'date');
+				});
+			resNotes = res.body[0];
+			return Notes.findById(resNotes.id).exec();
+			})
+		.then(function(notes){
+
+			resNotes.title.should.equal(notes.title);
+			resNotes.content.should.equal(notes.content);
+			resNotes.date.should.equal(notes.content);
+		});
+	});
+});
+
+	describe('POST endpoint', function(){
+
+		const newNote = {
+			title: faker.lorem.sentence(),
+			content: faker.lorem.text(),
+			date: faker.lorem.date()
+		}
+	});
+
+	return chai.request(app)
+	.post('/notes')
+	.send(newNote)
+	.then(function(res){
+		res.should
 	})
 
 })
